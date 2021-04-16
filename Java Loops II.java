239312b8e1aaa805d64cecbb88d13e6f827142ac
Sahil Phalle <46884132+spidy7977@users.noreply.github.com>
2021-04-16 23:07:23 +0530

    Day 2

---
+++ Java Loops II.java
@@ -0,0 +1,25 @@
+#Question Link : https://www.hackerrank.com/challenges/java-loops/problem?h_r=next-challenge&h_v=zen
+
+#Solution :-
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int res = a;
+            for (int j = 0; j < n; j++)
+            {
+                res += (int)(Math.pow(2, j) * b);
+                System.out.print(Integer.toString(res)  + ' ');
+            }
+            System.out.print('\n');
+        }
+        in.close();
+    }
+}
