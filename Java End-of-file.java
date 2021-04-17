adcd806dd36f0ab75305029a8fb8000bd353f2da
Sahil Phalle <46884132+spidy7977@users.noreply.github.com>
2021-04-17 13:25:24 +0530

    Day 3

---
+++ Java End-of-file.java
@@ -0,0 +1,25 @@
+// Question Link : https://www.hackerrank.com/challenges/java-end-of-file/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+// Solution :-
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i=0;
+        while(scan.hasNextLine()){
+        String str=scan.nextLine();
+            if (str.contains("end-of-file")){
+                i++;
+                System.out.println(i+" "+str);
+                break;
+            }else{
+                i++;
+                System.out.println(i+" " +str);       
+            }
+        }  
+    }
+}
